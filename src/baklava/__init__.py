03da9fbc19ef4bd7a19ca4eae6ae7f9a275ed818
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2024-09-03 14:02:07 +0100

    adding all files to repo

---
+++ src/baklava/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about baklava."""
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "David Salvador-Jasin"
+__email__ = "dsalvadorjasin@turing.ac.uk"
+__version__ = "0.1.0"
